7057515e63d5493f28f7785a16d8812e98e12cb9
jsemalvarez <jsemalvarez@gmail.com>
2026-03-23 15:19:49 -0300

    fix: globals css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 import { Providers } from "@/components/Providers";
 import { headers } from "next/headers";
 import { getTenantByHost } from "@/lib/tenant";
